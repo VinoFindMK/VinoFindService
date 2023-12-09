3376275b05b64f53dc35201097b9c02a121c4cce
LifesGoood <dragan.velinov@students.finki.ukim.mk>
2023-12-09 18:33:32 +0100

    made details page look better

--- src/main/java/com/ukim/finki/domashna2/service/WineryDataExporterService.java
+++ src/main/java/com/ukim/finki/domashna2/service/WineryDataExporterService.java
@@ -33,19 +33,19 @@ public class WineryDataExporterService implements CommandLineRunner {
             String apiKey = dotenv.get("GOOGLE_MAPS_API_KEY");
             GeoApiContext context = new GeoApiContext.Builder().apiKey(apiKey).build();
             LatLng[] locations = {
-//                new LatLng(41.25303, 21.27507),
-//                new LatLng(41.52297, 22.35999),
-//                new LatLng(41.93242, 21.69960),
-//                new LatLng(42.14813, 22.27066),
-//                new LatLng(41.83265, 20.95572),
-//                new LatLng(41.45005, 22.22156),
-//                new LatLng(41.75242, 22.55919),
-//                new LatLng(42.11101, 22.18597),
-//                new LatLng(41.81062, 21.90612),
-//                new LatLng(41.53209, 21.58333),
-//                new LatLng(41.07832, 20.96742),
-//                new LatLng(41.69503, 20.89885),
-//                new LatLng(42.05295, 21.33640),
+                new LatLng(41.25303, 21.27507),
+                new LatLng(41.52297, 22.35999),
+                new LatLng(41.93242, 21.69960),
+                new LatLng(42.14813, 22.27066),
+                new LatLng(41.83265, 20.95572),
+                new LatLng(41.45005, 22.22156),
+                new LatLng(41.75242, 22.55919),
+                new LatLng(42.11101, 22.18597),
+                new LatLng(41.81062, 21.90612),
+                new LatLng(41.53209, 21.58333),
+                new LatLng(41.07832, 20.96742),
+                new LatLng(41.69503, 20.89885),
+                new LatLng(42.05295, 21.33640),
                 new LatLng(41.09672, 21.60100)
             };
             int radius = 50000;
